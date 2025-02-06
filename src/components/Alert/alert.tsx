38b277b756cfeaf3c31a4b8f37b8700d708e5cbb
rock-on <noblemathewtm@gmail.com>
2025-02-06 20:11:42 +1100

    Fix styling issues

--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -1,29 +1,50 @@
-import Box from "@mui/material/Box"
+import Box from "@mui/material/Box";
 import { ReactNode } from "react";
-import Alert, { AlertProps } from '@mui/material/Alert';
+import Alert, { AlertProps } from "@mui/material/Alert";
 import { SEVERITY } from "../../constants/severity";
 import { VARIANT } from "../../constants/variant";
 import { AlertGlobalProps } from "context/alertContext";
 
 const horizontalPositionFlexProps = {
-    "center": "center",
-    "left": "flex-start",
-    "right": "flex-end"
+  center: "center",
+  left: "flex-start",
+  right: "flex-end",
+};
+
+export interface MuiAlertProps extends Omit<AlertProps, "onClose"> {
+  message: ReactNode | string;
+  inout?: number;
+  severity?: SEVERITY;
+  variant?: VARIANT;
+  onClose?: () => void;
+  globalProps?: AlertGlobalProps;
 }
 
-export interface MuiAlertProps extends Omit<AlertProps, 'onClose'> {
-    message: ReactNode | string
-    inout?: number,
-    severity?: SEVERITY,
-    variant?: VARIANT,
-    onClose?: () => void,
-    globalProps?: AlertGlobalProps
-}
-
-export const MuiAlert = ({ message, severity = 'success', onClose, variant = 'standard', globalProps, ...rest }: MuiAlertProps) => {
-    return (
-        <Box my="8px" display="flex" justifyContent={horizontalPositionFlexProps[globalProps?.horizontal || 'center']}>
-            <Alert severity={severity} variant={variant} {...globalProps} {...rest} onClose={onClose}>{message} </Alert>
-        </Box>
-    );
-}
+export const MuiAlert = ({
+  message,
+  severity = "success",
+  onClose,
+  variant = "standard",
+  globalProps,
+  ...rest
+}: MuiAlertProps) => {
+  return (
+    <Box
+      my="8px"
+      display="flex"
+      justifyContent={
+        horizontalPositionFlexProps[globalProps?.horizontal || "center"]
+      }
+    >
+      <Alert
+        severity={severity}
+        variant={variant}
+        {...globalProps}
+        {...rest}
+        onClose={onClose}
+      >
+        {message}{" "}
+      </Alert>
+    </Box>
+  );
+};
--- src/constants/position.ts
+++ src/constants/position.ts
@@ -1,5 +1,14 @@
-export type HORIZONTAL = 'center' | 'left' | 'right';
+export type HORIZONTAL = "center" | "left" | "right";
 
-export type VERTICAL = 'bottom' | 'top';
+export type VERTICAL = "bottom" | "top";
 
-export type CONFIRM_DIALOG_POSITIONS = 'top-left' | 'top-center' | 'top-right' | 'center-left' | 'center-center' | 'center-right' | 'bottom-left' | 'bottom-center' | 'bottom-right';
+export type CONFIRM_DIALOG_POSITIONS =
+  | "top-left"
+  | "top-center"
+  | "top-right"
+  | "center-left"
+  | "center-center"
+  | "center-right"
+  | "bottom-left"
+  | "bottom-center"
+  | "bottom-right";
